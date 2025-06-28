e1dc166478368a9e735df6076f4f7cb6738c2b10
Stephanie Spedowski <millmine2023@gmail.com>
2025-06-27 23:16:57 -0400

    commit

--- justice-dashboard/postcss.config.js
+++ justice-dashboard/postcss.config.js
@@ -2,12 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {
-      browsers: [
-        'last 2 versions',
-        'not dead',
-        'not ie <= 11',
-        'not op_mini all'
-      ],
       flexbox: 'no-2009',
       grid: 'autoplace'
     }
