1e7ac682e2f5954bdd36901c0ca22fe36b4ee90e
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-01 01:42:25 -0400

    commit

---
+++ justice-dashboard/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': 'jest-transform-stub'
+  },
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/**/*.(test|spec).{js,jsx,ts,tsx}'
+  ],
+  collectCoverageFrom: [
+    'frontend/**/*.{js,jsx}',
+    'backend/**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html']
+};
