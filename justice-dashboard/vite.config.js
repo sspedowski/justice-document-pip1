2614f6646cc7c4481aa0881b6222a425bf2f17e1
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-01 18:57:16 -0400

    commit

--- justice-dashboard/vite.config.js
+++ justice-dashboard/vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/upload': 'http://localhost:4000',
-      '/api': 'http://localhost:4000',
+      '/upload': 'http://localhost:3000',
+      '/api': 'http://localhost:3000',
     },
   },
   build: {
