e00c4a00d38a0ff1976b2cbbe141b30152b056de
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-13 21:04:44 -0400

    commit

---
+++ hash-password.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+
+async function hashPassword(password) {
+  try {
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(password, saltRounds);
+    console.log(`Password: ${password}`);
+    console.log(`Hash: ${hash}`);
+    return hash;
+  } catch (error) {
+    console.error('Error hashing password:', error);
+  }
+}
+
+// Hash guest password
+const password = process.argv[2] || 'guest123';
+hashPassword(password);
