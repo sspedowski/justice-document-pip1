61ae5853de97cc68a17fd7851faff53f4efd4c62
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-18 19:44:18 -0400

    COMMIT

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      // Add your custom rules here
+    },
+  },
+];
