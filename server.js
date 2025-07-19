0805a7cd2fda45f20fd407602c584a2612004ad1
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-18 20:23:42 -0400

    commit

--- server.js
+++ server.js
@@ -78,7 +78,9 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:5174",
       "http://localhost:5175",
+      "http://localhost:5176",
       "http://localhost:5177",
       "http://localhost:3000",
       "https://sspedowski.github.io",
