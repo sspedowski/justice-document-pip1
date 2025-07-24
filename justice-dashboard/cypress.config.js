408e6c8261c6a1c9ed6c3cdd20646c9eef019a17
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-24 19:28:47 -0400

    COMMIT

--- justice-dashboard/cypress.config.js
+++ justice-dashboard/cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'mbvhmn',
   e2e: {
     baseUrl: 'http://localhost:5174',
     supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.js',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
   },
 });
