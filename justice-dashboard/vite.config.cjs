29ccbda69b584aa4a5e35df1a5a021e72372c675
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-27 14:00:07 -0400

    COMMIT

--- justice-dashboard/vite.config.cjs
+++ justice-dashboard/vite.config.cjs
@@ -1,10 +1,8 @@
-const react = require('@vitejs/plugin-react');
 const { defineConfig } = require('vite');
 const path = require('path');
 
 module.exports = defineConfig({
   root: path.resolve(__dirname, '..'),
-  plugins: [react()],
   build: {
     outDir: 'justice-dashboard/dist'
   },
