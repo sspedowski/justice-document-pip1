4a5dd6c644e71301abc1fbb1bfac5e10d12a74f4
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-30 02:49:54 -0400

    commit

--- start-local.js
+++ start-local.js
@@ -2,8 +2,10 @@ const { spawn } = require("child_process");
 
 console.log("🚀 Starting Justice Dashboard locally...");
 
+
+const npmCmd = process.platform === "win32" ? "npm.cmd" : "npm";
 const server = spawn("node", ["server.js"], { cwd: "./justice-server", stdio: "inherit" });
-const client = spawn("npm", ["run", "dev"], { cwd: "./justice-dashboard", stdio: "inherit" });
+const client = spawn(npmCmd, ["run", "dev"], { cwd: "./justice-dashboard", stdio: "inherit" });
 
 process.on("SIGINT", () => {
   console.log("\n🛑 Stopping...");
