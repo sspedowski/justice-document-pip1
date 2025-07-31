1325de94bb4d05d3439e9f72bc7e4872caca80e6
Stephanie Spedowski <millmine2023@gmail.com>
2025-07-31 14:09:13 -0400

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,25 @@
 module.exports = {
   env: {
+    browser: true,    // <-- add browser for React apps
     node: true,
     es2021: true,
   },
-  extends: ["eslint:recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"   // <-- add React plugin
+  ],
+  plugins: ["react"],            // <-- add React plugin
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true                 // <-- enable JSX parsing
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"         // <-- auto-detect React version
+    }
   },
   rules: {},
 };
