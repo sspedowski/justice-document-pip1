90d4bc73bd19ebc9a542176dd0b8016dac1129a0
Stephanie Spedowski <millmine2023@gmail.com>
2025-08-27 13:20:03 -0400

    commit

--- justice-dashboard/eslint.config.js
+++ justice-dashboard/eslint.config.js
@@ -1,29 +1,4 @@
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import hooks from "eslint-plugin-react-hooks";
-
-export default [
-  {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    ignores: ["dist/**", "node_modules/**", "build/**"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parserOptions: {
-        ecmaFeatures: { jsx: true }
-      }
-    },
-    plugins: { react, "react-hooks": hooks },
-    rules: {
-      ...js.configs.recommended.rules,
-      // react plugin recommended rules
-      ...(react && react.configs && react.configs.recommended ? react.configs.recommended.rules : {}),
-      ...(hooks && hooks.configs && hooks.configs.recommended ? hooks.configs.recommended.rules : {})
-    },
-    settings: { react: { version: "detect" } }
-  }
-];
-import js from "@eslint/js";
 import babelParser from "@babel/eslint-parser";
 import react from "eslint-plugin-react";
 import hooks from "eslint-plugin-react-hooks";
@@ -31,38 +6,21 @@ import a11y from "eslint-plugin-jsx-a11y";
 import importPlugin from "eslint-plugin-import";
 
 export default [
-  // Equivalent to "eslint:recommended"
   js.configs.recommended,
-
-  // Equivalent to "plugin:react/recommended"
-  react.configs.recommended,
-
-  // App source files
   {
-    files: ["**/*.{js,jsx,mjs,cjs}"],
-    ignores: ["node_modules/", "dist/", "build/"],
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    ignores: ["node_modules/**", "dist/**", "build/**"],
     languageOptions: {
       ecmaVersion: 2023,
       sourceType: "module",
       parser: babelParser,
       parserOptions: {
         requireConfigFile: false,
-        babelOptions: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
+        babelOptions: { presets: ["@babel/preset-env", "@babel/preset-react"] },
       },
-      globals: {
-        window: "readonly",
-        document: "readonly",
-        navigator: "readonly",
-      },
+      globals: { window: "readonly", document: "readonly", navigator: "readonly" },
     },
-    plugins: {
-      react,
-      "react-hooks": hooks,
-      "jsx-a11y": a11y,
-      import: importPlugin,
-    },
+    plugins: { react, "react-hooks": hooks, "jsx-a11y": a11y, import: importPlugin },
     settings: { react: { version: "detect" } },
     rules: {
       // Core
@@ -81,34 +39,20 @@ export default [
       "jsx-a11y/no-autofocus": "warn",
 
       // Imports
-      "import/order": ["warn", {
-        groups: [["builtin", "external", "internal"], ["parent", "sibling", "index"]],
-        "newlines-between": "always",
-        alphabetize: { order: "asc", caseInsensitive: true },
-      }],
+      "import/order": [
+        "warn",
+        {
+          groups: [["builtin", "external", "internal"], ["parent", "sibling", "index"]],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
       "import/no-duplicates": "warn",
     },
   },
-
-  // (Optional) Cypress tests
+  // Cypress or test files (if present)
   {
     files: ["cypress/**/*.js", "cypress.config.*"],
-    languageOptions: {
-      // If your config is ESM, keep sourceType: "module"
-      sourceType: "module",
-      globals: { cy: "readonly", Cypress: "readonly" },
-    },
-    rules: {},
-  },
-
-  // (Optional) Project config files (eslint/vite/etc.)
-  {
-    files: ["eslint.config.js", "*.config.{js,cjs,mjs}"],
-    languageOptions: {
-      ecmaVersion: 2023,
-      sourceType: "module",
-      globals: { require: "readonly", module: "readonly", __dirname: "readonly" },
-    },
-    rules: {},
+    languageOptions: { sourceType: "module", globals: { cy: "readonly", Cypress: "readonly" } },
   },
 ];
