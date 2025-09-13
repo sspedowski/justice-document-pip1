a7ba44ee6fbc868860de6b328b0eb8931caf146e
Stephanie Spedowski <millmine2023@gmail.com>
2025-09-13 00:40:06 -0400

    COMMIT

--- next.config.cjs
+++ next.config.cjs
@@ -1,7 +1,15 @@
+/// <reference types="next" />
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
+  // TEMP: unblock builds until ESLint is happy; remove later
   eslint: { ignoreDuringBuilds: true },
-  swcMinify: true,
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+    reactRemoveProperties: true,
+  },
 };
 
 module.exports = nextConfig;
